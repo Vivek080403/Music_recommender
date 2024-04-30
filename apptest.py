f705370f3d84b9f04febfd33668aec70cdf0f395
Vivek <vpsr9246@gmail.com>
2024-04-30 14:09:48 +0530

    first commit

---
+++ apptest.py
@@ -0,0 +1,159 @@
+import pickle
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import cv2
+import av
+import numpy as np 
+import mediapipe as mp 
+from keras.models import load_model
+import webbrowser
+
+model  = load_model("model.h5")
+label = np.load("labels.npy")
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+holis = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+
+CLIENT_ID = "7fde27fc83884275aa128154161788df"
+CLIENT_SECRET = "5a4cfce5aef94019b7a1a1c14e0dd6c4"
+
+# Initialize the Spotify client
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_song_album_cover_url(song_name, artist_name):
+    search_query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=search_query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        print(album_cover_url)
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+class EmotionProcessor:
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
+		frm = cv2.flip(frm, 1)
+
+		res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+
+		lst = []
+
+		if res.face_landmarks:
+			for i in res.face_landmarks.landmark:
+				lst.append(i.x - res.face_landmarks.landmark[1].x)
+				lst.append(i.y - res.face_landmarks.landmark[1].y)
+
+			if res.left_hand_landmarks:
+				for i in res.left_hand_landmarks.landmark:
+					lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+					lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+			else:
+				for i in range(42):
+					lst.append(0.0)
+
+			if res.right_hand_landmarks:
+				for i in res.right_hand_landmarks.landmark:
+					lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+					lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+			else:
+				for i in range(42):
+					lst.append(0.0)
+
+			lst = np.array(lst).reshape(1,-1)
+
+			pred = label[np.argmax(model.predict(lst))]
+
+			print(pred)
+			cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+
+			np.save("emotion.npy", np.array([pred]))
+
+			
+		drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+								landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
+								connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+		drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+		drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+
+
+
+		return av.VideoFrame.from_ndarray(frm, format="bgr24")
+st.header('Mood-Based Music Recommendation')
+lang = st.text_input("Language")
+singer = st.text_input("singer")
+
+if lang and singer and st.session_state["run"] != "false":
+	webrtc_streamer(key="key", desired_playing_state=True,
+				video_processor_factory=EmotionProcessor)
+
+if "run" not in st.session_state:
+	st.session_state["run"] = "true"
+
+try:
+	emotion = np.load("emotion.npy")[0]
+except:
+	emotion=""
+
+if st.button:
+	if not(emotion):
+		st.warning("Please let me capture your emotion first")
+		st.session_state["run"] = "true"
+	else:
+		webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
+		np.save("emotion.npy", np.array([""]))
+		st.session_state["run"] = "false"
+def recommend(title):
+    index = music[music['title'] == title].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+	
+    for i in distances[1:6]:
+        # fetch the movie poster
+        artist = music.iloc[i[0]].artist
+        print(artist)
+        print(music.iloc[i[0]].title)
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].title, artist))
+        recommended_music_names.append(music.iloc[i[0]].title)
+
+    return recommended_music_names,recommended_music_posters
+
+
+music = pickle.load(open('df.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+music_list = music['title'].values
+selected_music = st.selectbox(
+    "Type or select a song from the dropdown",
+    music_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_music_names,recommended_music_posters = recommend(selected_music)
+    col1, col2, col3, col4, col5= st.columns(5)
+    with col1:
+        st.text(recommended_music_names[0])
+        st.image(recommended_music_posters[0])
+    with col2:
+        st.text(recommended_music_names[1])
+        st.image(recommended_music_posters[1])
+
+    with col3:
+        st.text(recommended_music_names[2])
+        st.image(recommended_music_posters[2])
+    with col4:
+        st.text(recommended_music_names[3])
+        st.image(recommended_music_posters[3])
+    with col5:
+        st.text(recommended_music_names[4])
+        st.image(recommended_music_posters[4])
+
+
+
+
